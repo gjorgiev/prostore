35bbf177e61b720373d43a20894256259ee15ecd
Georgi Georgiev <georgigeorgiev@iMac-von-Georgi.fritz.box>
2025-06-14 17:38:25 +0200

    zod validation

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -3,7 +3,6 @@ import { PrismaClient } from '../generated/prisma'
 import { convertToPlainObject } from '../utils'
 import { LATEST_PRODUCTS_LIMIT } from '../constants'
 
-// Get latest products
 export async function getLatestProducts() {
   const prisma = new PrismaClient()
 
---
+++ types/index.ts
@@ -0,0 +1,8 @@
+import type { z } from 'zod'
+import type { insertProductSchema } from '@/lib/validators'
+
+export type Product = z.infer<typeof insertProductSchema> & {
+  id: string
+  rating: string
+  createdAt: Date
+}
